3b8faa6632044088c3148ed712f182fd5bbc9a2a
Adi Pedel <adipedel@gmail.com>
2025-05-12 00:40:56 +0300

    is_palindrome function

--- mmn14.py
+++ mmn14.py
@@ -19,7 +19,9 @@ The function will call herself until it will find the maximum value (recursion).
 Because each function call reduces the list size by half, the running time is logarithmic.
 """
 def find_max(lst):
-    if len(lst)==1:
+    if len(lst) == 0:
+        return None
+    if len(lst) == 1:
         return lst[0]
     if len(lst) == 2:
         if lst[0]<lst[1]:
@@ -65,18 +67,21 @@ def find_pairs(lst,k):
 """
 update_list is a function that receives a list of numbers and a value. It will return the list without the
 given value using recursion. If the value appears in the list more than once, only the first item with that value will be
-removed from the list.
+removed from the list. 
 """
 def update_list(lst,value):
-    if lst[0] == value:
+    if len(lst) == 0:
+        return []
+    if lst[0] == value:     #the value we want to remove is in the start of the list
         if len(lst) == 1:
-            return []
+            return []   #if it's the last item in the list, we will return an empty list
         else:
-            return lst[1::]
-    elif len(lst) == 1:
+            return lst[1::] #it's not the end of the list, removing the item by returning the list starting the next item
+    elif len(lst) == 1:     #if it's the last item of the list, and it's not the value, returning the list
         return [lst[0]]
     else:
         return [lst[0]]+update_list(lst[1::], value)
+#calling the function with the list starting with the next item to check if the value is in there, and returning the resulted list
 
 """
 equal_lists is a function that receives two list of numbers. The function will return True if the lists
@@ -85,15 +90,68 @@ out a value from the two list recursively until one of the lists is empty. If th
 in size each call, they meet the conditions and the function will return True.
 """
 def equal_lists(lst1, lst2):
-    if lst1 == lst2 == []:
+    if lst1 == lst2 == []:  #if the two lists are empty, they are equal and the function returns True
         return True
-    elif lst1 == [] or lst2 == []:
+    elif len(lst1) != len(lst2):    #the lists are not equal in size, the function returns False
         return False
     else:
-        value = lst1[0]
-        lst1 = update_list(lst1, value)
+        value = lst1[0]         #the value we will try to remove from the list
+        lst1 = update_list(lst1, value)     #calling the function to remove the value and updating the new lists
         lst2 = update_list(lst2, value)
-        result = equal_lists(lst1, lst2)
+        result = equal_lists(lst1, lst2)    #calling the function again with the updated lists
         return result
 
 
+"""
+is_palindrome is a function that receives a list of strings and return true if each list is a palindrome
+and together they formed a palindrome. the function does it with recursion,it compares the string
+in the beginning and in the end of the list, and if the chars in the begging and end of each string are the
+same it trims them and with recursion continue to compare them until the strings are empty. the  function
+continues too checks every string until it detect a string that isn't a palindrome or doesnt create a
+palindrome in thr whole list or if the list is empty.
+
+"""
+def is_palindrome(lst):
+    if len(lst) == 0:
+        return True
+    if len(lst) == 1 :
+        if len(lst[0]) == 1:
+            return True
+        if len(lst[0]) == 2:
+            if lst[0][0] == lst[0][-1]:
+                return True
+            else:
+                return False
+        else:
+            if lst[0][0] == lst[0][-1]:
+                lst[0] = lst[0][1:-1]
+                if is_palindrome(lst):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+    else:
+        if len(lst[0]) != len(lst[-1]):
+            return False
+        if len(lst[0]) == 0 or  len(lst[0]) == 1:
+            lst = lst[1:-1]
+            if is_palindrome(lst):
+                return True
+            else:
+                return False
+        if lst[0][0] == lst[0][-1] == lst[-1][0] == lst[-1][-1]:
+            if len(lst[0]) == 2:
+                lst = lst[1:-1]
+                if is_palindrome(lst):
+                    return True
+                else:
+                    return False
+            else:
+                lst[0], lst[-1] = lst[0][1:-1], lst[-1][1:-1]
+                if is_palindrome(lst):
+                    return True
+                else:
+                    return False
+        else:
+            return False
